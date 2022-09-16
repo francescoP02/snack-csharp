e0f0c1b240e9d8831ad6e3d6f5f6e16dcee29422
francescoP02 <file.partipilo@gmail.com>
2022-09-16 14:20:41 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Snack 1
+
+//Console.WriteLine("Inserisci un numero");
+
+//int numero1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Inserisci un secondo numero");
+
+//int numero2 = Convert.ToInt32(Console.ReadLine());
+
+//if (numero1 > numero2)
+//{
+//    Console.WriteLine($"Il numero maggiore è {numero1}");
+//} else if (numero1 < numero2)
+//{
+//    Console.WriteLine($"Il numero maggiore è {numero2}");
+//} else
+//{
+//    Console.WriteLine("I due numeri sono uguali");
+//}
